3590eec9bcf55f0e263f9126d089e988e28d06c9
arajaramanca <arajaraman@arajaraman-mbr-2.local>
2016-08-01 16:54:56 -0400

    creating quarter selector widget

---
+++ quarter-selector/src/javascript/app.js
@@ -0,0 +1,46 @@
+Ext.define("QSApp", {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    logger: new Rally.technicalservices.Logger(),
+    defaults: { margin: 10 },
+    integrationHeaders : {
+        name : "QSApp"
+    },
+
+    launch: function() {
+        this.removeAll();
+
+        this.add({
+            xtype: 'quarteritemselector',
+            stateId: this.getContext().getScopedStateId('app-selector'),
+            flex: 1
+        });
+      
+    },
+      
+    getOptions: function() {
+        return [
+            {
+                text: 'About...',
+                handler: this._launchInfo,
+                scope: this
+            }
+        ];
+    },
+    
+    _launchInfo: function() {
+        if ( this.about_dialog ) { this.about_dialog.destroy(); }
+        this.about_dialog = Ext.create('Rally.technicalservices.InfoLink',{});
+    },
+    
+    isExternal: function(){
+        return typeof(this.getAppId()) == 'undefined';
+    },
+    
+    //onSettingsUpdate:  Override
+    onSettingsUpdate: function (settings){
+        this.logger.log('onSettingsUpdate',settings);
+        // Ext.apply(this, settings);
+        this.launch();
+    }
+});
