a430c4a6853a8162ec5b879dfb38e8dcbc3dead4
John Martin <curmudgeon+git@rallydev.com>
2016-09-22 19:15:23 -0700

    fix style of oi-productivity pull out

--- oi-productivity/Gruntfile.js
+++ oi-productivity/Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
 
         config.ugly_files = grunt.file.expand(['deploy/app.min.*.js']);
         
-        config.css_files = grunt.file.expand( 'src/style/*.css' );
+        config.css_files = grunt.file.expand( ['../common/src/style/*.css','src/style/*.css']);
         
         config.js_contents = " ";
         for (var i=0;i<config.js_files.length;i++) {
