fa6d16f28f25c27d1abc9802f00c9cf2fd27a6c5
John Martin <curmudgeon+git@rallydev.com>
2016-10-23 19:18:53 -0700

    change labeling on integrity

--- common/src/javascript/ts-grid-box.js
+++ common/src/javascript/ts-grid-box.js
@@ -1,17 +1,23 @@
 Ext.define('CA.techservices.container.GridBox',{
     extend: 'Ext.container.Container',
     alias:  'widget.tsgridbox',
-    
-    layout: 'border',
-    
+        
     items: [
-        {xtype:'container', region: 'center', layout:'fit', itemId:'grid_box'},
+        {xtype:'container', region: 'center', itemId:'grid_box'},
     ],
     
     setGrid: function(config) {
+        var default_config = {
+            margin: 10,
+            padding: 10,
+            height: 200
+        };
+                
         var box = this.down('#grid_box');
         box.removeAll();
         
-        box.add(config);
+        var new_config = Ext.Object.merge(default_config,config);
+        
+        box.add(new_config);
     }
 });
